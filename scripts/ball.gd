8a4c807f561b362ef5a068f2aaec473dfbcdf8e4
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2025-09-12 17:56:47 -0400

    Get ball to work properly and add asset

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -1,24 +1,21 @@
-extends RigidBody2D
+extends CharacterBody2D
+
+var collision: KinematicCollision2D
 
 @export var initial_speed: float = 400.0
-@export var gravityModifier: float = 0.0
-
-# func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
-#     rotation_degrees = 0
 
 func _ready() -> void:
-	lock_rotation  = true
 	randomize()
 	if randi() % 2 == 0:
 		initial_speed = -initial_speed
-		gravity_scale = gravityModifier * -1.0
-	apply_central_impulse(Vector2(initial_speed, 0))
+	velocity = Vector2(initial_speed, 0)
 
 
-
-func _on_body_entered(body: Node) -> void:
-	print("Collided with: ", body.name)
-	if body.is_in_group("player"):
-		print("Hit Player")
-		print("Gravity Scale Before: ", gravity_scale)
-		gravity_scale = gravity_scale * -1.0
+func _physics_process(delta: float) -> void:
+	collision = move_and_collide(velocity * delta)
+	if collision:
+		var normal = collision.get_normal()
+		var colVelocity = collision.get_collider_velocity()
+		if colVelocity != Vector2.ZERO:
+			velocity += colVelocity
+		velocity = velocity.bounce(normal)
